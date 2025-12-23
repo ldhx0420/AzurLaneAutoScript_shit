df496ab49bcc074f19df3a86c875f835a1d7660c
ldhx <1362096583@qq.com>
2025-12-24 00:35:50 +0800

    Merge pull request #5378 from LmeSzinc/dev
    Bug fix

--- module/island/island_manufacture.py
+++ module/island/island_manufacture.py
@@ -131,7 +131,7 @@ class IslandManufacture(IslandShopBase):
                 self.select_character()
                 self.appear_then_click(SELECT_UI_CONFIRM)
                 continue
-            if self.appear(ISAND_SELECT_PRODUCT_CHECK, offset=1):
+            if self.appear(ISLAND_SELECT_PRODUCT_CHECK, offset=1):
                 selected_product = None
                 for product_info in product_list:
                     product_name = product_info['name']
