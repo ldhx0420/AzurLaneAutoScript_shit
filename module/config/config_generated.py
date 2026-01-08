a99c169e85c24d5554c6a3c4eba3dc351214b5d9
ldhx <1362096583@qq.com>
2026-01-09 03:31:32 +0800

    Merge pull request #5378 from LmeSzinc/dev
    Bug fix

--- module/config/config_generated.py
+++ module/config/config_generated.py
@@ -472,7 +472,7 @@ class GeneratedConfig:
 
     # Group `IslandRestaurant`
     IslandRestaurant_PostNumber = 2  # 1, 2
-    IslandRestaurant_Chef = 'WorkerJuu'
+    IslandRestaurant_ChefFilter = 'WorkerJuu'
     IslandRestaurant_Meal1 = 'tofu_meat'  # None, tofu, omurice, cabbage_tofu, salad, tofu_meat, tofu_combo, hearty_meal
     IslandRestaurant_MealNumber1 = 6
     IslandRestaurant_Meal2 = 'hearty_meal'  # None, tofu, omurice, cabbage_tofu, salad, tofu_meat, tofu_combo, hearty_meal
@@ -495,7 +495,7 @@ class GeneratedConfig:
 
     # Group `IslandTeahouse`
     IslandTeahouse_PostNumber = 2  # 1, 2
-    IslandTeahouse_Chef = 'WorkerJuu'
+    IslandTeahouse_ChefFilter = 'WorkerJuu'
     IslandTeahouse_Meal1 = 'floral_fruity'  # None, apple_juice, banana_mango, honey_lemon, strawberry_lemon, strawberry_honey, floral_fruity, fruit_paradise, lavender_tea, sunny_honey
     IslandTeahouse_MealNumber1 = 6
     IslandTeahouse_Meal2 = 'lavender_tea'  # None, apple_juice, banana_mango, honey_lemon, strawberry_lemon, strawberry_honey, floral_fruity, fruit_paradise, lavender_tea, sunny_honey
@@ -518,7 +518,7 @@ class GeneratedConfig:
 
     # Group `IslandGrill`
     IslandGrill_PostNumber = 1  # 1, 2
-    IslandGrill_Chef = 'WorkerJuu'
+    IslandGrill_ChefFilter = 'WorkerJuu'
     IslandGrill_Meal1 = 'double_energy'  # None, roasted_skewer, chicken_potato, carrot_omelette, stir_fried_chicken, steak_bowl, carnival, double_energy
     IslandGrill_MealNumber1 = 6
     IslandGrill_Meal2 = 'steak_bowl'  # None, roasted_skewer, chicken_potato, carrot_omelette, stir_fried_chicken, steak_bowl, carnival, double_energy
@@ -541,7 +541,7 @@ class GeneratedConfig:
 
     # Group `IslandJuuEatery`
     IslandJuuEatery_PostNumber = 1  # 1, 2
-    IslandJuuEatery_Chef = 'WorkerJuu'
+    IslandJuuEatery_ChefFilter = 'WorkerJuu'
     IslandJuuEatery_Meal1 = 'berry_orange'  # None, apple_pie, corn_cup, orange_pie, banana_crepe, orchard_duo, rice_mango, succulently_sweet, berry_orange, strawberry_charlotte
     IslandJuuEatery_MealNumber1 = 6
     IslandJuuEatery_Meal2 = 'succulently_sweet'  # None, apple_pie, corn_cup, orange_pie, banana_crepe, orchard_duo, rice_mango, succulently_sweet, berry_orange, strawberry_charlotte
@@ -564,7 +564,7 @@ class GeneratedConfig:
 
     # Group `IslandJuuCoffee`
     IslandJuuCoffee_PostNumber = 2  # 1, 2
-    IslandJuuCoffee_Chef = 'WorkerJuu'
+    IslandJuuCoffee_ChefFilter = 'WorkerJuu'
     IslandJuuCoffee_Meal1 = 'wake_up_call'  # None, iced_coffee, omelette, cheese, latte, citrus_coffee, strawberry_milkshake, morning_light, wake_up_call, fruity_fruitier
     IslandJuuCoffee_MealNumber1 = 6
     IslandJuuCoffee_Meal2 = 'cheese'  # None, iced_coffee, omelette, cheese, latte, citrus_coffee, strawberry_milkshake, morning_light, wake_up_call, fruity_fruitier
